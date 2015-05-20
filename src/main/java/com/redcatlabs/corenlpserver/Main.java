abaa8837398b33ca74f29890252015349e0da9b4
Martin Andrews <GitHub@mdda.net>
2015-05-20 18:27:01 +0800

    No need for --help property parsing

--- src/main/java/com/redcatlabs/corenlpserver/Main.java
+++ src/main/java/com/redcatlabs/corenlpserver/Main.java
@@ -5,6 +5,9 @@ import static spark.Spark.*;
 //ResponseTransformer  :: To json
 import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.StringUtils;
@@ -12,9 +15,9 @@ import edu.stanford.nlp.util.StringUtils;
 public class Main {
 
     // See : http://sparkjava.com/documentation.html
-    public static void main(String[] args) {
-        port(4567);  //  Default would be 4567
-
+    private static void runServer(final Properties props, final int server_port) {
+        port(server_port);  
+        
         Gson gson = new Gson();
 
         // Initial connection will be on http://127.0.0.1:4567/hello
@@ -28,6 +31,24 @@ public class Main {
         
         // ResponseTransformer  :: To json
         // get("/hello", (request, response) -> new MyMessage("Hello World"), gson::toJson);
+        
+        // CoreNLP also has : public void jsonPrint(Annotation annotation, Writer w) throws IOException
+
     }
     
+    public static void main(String[] args) throws IOException {
+        Properties props = null;
+        
+        int server_port = 4567;  // This is SparkJava's default
+
+        if (args.length > 0) {
+            props = StringUtils.argsToProperties(args);
+            
+            final String portString = props.getProperty("port");
+            if (portString != null) {
+                server_port = Integer.parseInt(portString);
+            }
+        }
+        runServer(props, server_port);
+    }
 }
