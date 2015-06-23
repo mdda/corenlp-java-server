39d8eae3024377a9df6dab1e0d1f90e168ddfa94
Martin Andrews <GitHub@mdda.net>
2015-06-23 13:32:17 +0800

    NER doesn't require parse...

--- scripts/test_ner.bash
+++ scripts/test_ner.bash
@@ -1,15 +1,17 @@
 #! /usr/bin/bash
 
 curl -X POST http://localhost:4567/ner                         \
-  -d '{"doc":["Jack and Jill did nt go up the hill ."], 
+  -d '{"doc":["Jack and Jill did nt go up the hill .\nHowever , Jill fell down ."], 
        "props":{
-         "annotators":"tokenize,ssplit,pos,lemma,ner,parse",
+         "annotators":"tokenize,ssplit,pos,lemma,ner",
          "tokenize.whitespace":"true",
          "ssplit.eolonly":"true"
        }
       }'
 
 
+#         "annotators":"tokenize,ssplit,pos,lemma,ner,parse",
+#         "annotators":"tokenize,ssplit,pos,lemma,ner",
 # \nHowever , Jill fell down .
 
 
